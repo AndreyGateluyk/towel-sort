8ec0aa6ce88b410af2fc6dfa2cfc414824acd3c6
Andrey Gateluyk <gatelyuk97@mail.ru>
2023-10-24 22:16:52 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let matrixResult = []
+  if (matrix === undefined) {return matrixResult}
+  if (matrix.length < 1) {return matrixResult}
+  for (i = 0; i<=matrix.length-1; i++) {
+    if(i % 2 != 0) {
+      matrix[i] = matrix[i].reverse()
+    }
+    matrixResult = matrixResult.concat(matrix[i])
+  }
+  return matrixResult
 }
